9790ef9e2e09396b1227a1437a5426ccd80c52b7
santosral <sralphian029@gmail.com>
2021-05-04 20:25:34 +0800

    Class Confection Exercise

---
+++ confection.rb
@@ -0,0 +1,21 @@
+class Confection
+    def prepare
+      puts "Baking at 350 degrees for 25 minutes."
+    end
+  end
+  
+  class Cupcake < Confection
+    def prepare
+      super
+      puts "Applying frosting."
+    end
+  end
+
+  class Banana_Cake < Confection
+  end
+end
+
+  cupcake = Cupcake.new
+  bananacake = Banana_Cake.new
+  cupcake.prepare
+  bananacake.prepare
--- find_smallest_int.rb
+++ find_smallest_int.rb
@@ -1,7 +1,7 @@
 # arr = [34, 15, 88, 2]
 # arr = [34, -345, -1, 100]
 
-def find_smallest_int (arr)
+def find_smallest_int (arr) 
     smallest = arr[0]
 
     arr.each do |n|
